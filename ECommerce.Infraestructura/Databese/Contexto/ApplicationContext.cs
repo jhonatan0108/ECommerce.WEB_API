c9ddb4a1db027c7b3b08c5209c94d5d72e38903d
Jonathan Hurtado <jhontan.0108@gmail.com>
2023-06-06 21:13:54 -0500

    Agregar archivos de proyecto.

---
+++ ECommerce.Infraestructura/Databese/Contexto/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Infraestructura.Databese.Entidades.Clientes;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infraestructura.Databese.Contexto
+{
+    public class ApplicationContext : DbContext
+    {
+
+        #region DBSets
+        public virtual DbSet<ClienteEntities> Clientes { get; set;}
+
+        #endregion
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+    }
+}
---
+++ ECommerce.Infraestructura/Databese/Entidades/Clientes/ClienteEntities.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerce.Infraestructura.Databese.Entidades.Clientes
+{
+    [Table("Cliente")]
+    public class ClienteEntities
+    {
+        [Key]
+        public int id_cliente { get; set; }
+        public string nombre { get; set; } = string.Empty;
+        public string correo { get; set; } = string.Empty;
+        public string? password { get; set; } = string.Empty;
+        public decimal? telefono { get; set; }
+        public string? direccionfacturacion { get; set; } = string.Empty;
+    }
+}
